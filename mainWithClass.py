f82466c9f9d9aed98492dedbfdcea8b4fc8fa6a4
Toren C <theplatecrafter@gmail.com>
2023-08-20 17:13:22 +0900

    3

--- mainWithClass.py
+++ mainWithClass.py
@@ -4,6 +4,12 @@ class Complex():
   def __init__(self,real:float,imaginary:float):
     self.Re = real
     self.Im = imaginary
+    self.r = math.sqrt(pow(real,2)+pow(imaginary,2))
+    if imaginary >= 0:
+      self.theta =  math.acos(real/(math.sqrt(pow(real,2)+pow(imaginary,2))))
+    else:
+      self.theta =  2*math.pi-math.acos(real/(math.sqrt(pow(real,2)+pow(imaginary,2))))
+    self.thetaDEG = math.degrees(self.theta)
 
   def __str__(self):
     if self.Re == 0:
@@ -30,25 +36,44 @@ class Complex():
           return str(self.Re) + " + " + str(self.Im) + "i"
 
   def __add__(self,other):
-    return Complex(self.Re+other.Re,self.Im+other.Im)
-  
+    if type(other) == Complex:
+      return Complex(self.Re+other.Re,self.Im+other.Im)
+    else:
+      return Complex(self.Re+other,self.Im)
+
   def __sub__(self,other):
-    return Complex(self.Re-other.Re,self.Im-other.Im)
-  
+    if type(other) == Complex:
+      return Complex(self.Re-other.Re,self.Im-other.Im)
+    else:
+      return Complex(self.Re-other,self.Im)
+
   def __mul__(self,other):
-    return Complex(self.Re*other.Re-self.Im*other.Im, self.Im*other.Re+self.Re*other.Im)
+    if type(other) == Complex:
+      return Complex(self.Re*other.Re-self.Im*other.Im, self.Im*other.Re+self.Re*other.Im)
+    else:
+      return Complex(self.Re*other,self.Im*other)
   
   def __truediv__(self,other):
-    BDash = math.pow(other.Re,2) + math.pow(other.Im,2)
-    ADash = self * Complex(other.Re, -1*other.Im)
-    return Complex(ADash.Re/BDash, ADash.Im/BDash)
+    if type(other) == Complex:
+      BDash = math.pow(other.Re,2) + math.pow(other.Im,2)
+      ADash = self * Complex(other.Re, -1*other.Im)
+      return Complex(ADash.Re/BDash, ADash.Im/BDash)
+    else:
+      return Complex(self.Re/other,self.Im/other)
   
-  def __pow__(self,other:float):
+  def __pow__(self,other):
+    if type(other) == Complex:
+      x = (self.r**other.Im)*(math.e**(other.Re*self.theta))
+      y = (self.r**other.Re)*(math.e**(-1*self.theta*other.Im))
+      return Complex(y*math.cos(math.log(x)),y*math.sin(math.log(x)))
+    else:
+      r = self.r**other
+      theta = self.theta*other
+      return Complex(r*math.cos(theta),r*math.sin(theta))
     
-    
 
 
-a = Complex(1,1)
-b = Complex(2,0)
+a = Complex(2,1)
+b = Complex(2,5)
 
-print(a/b)
+print(a**b)
