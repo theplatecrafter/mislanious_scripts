c93fe268f1bedbc6fb933e1306b64cd4145f8f1b
theplatecrafter <theplatecrafter@gmail.com>
2023-08-30 19:00:03 +0900

    w

--- mainWithClass.py
+++ mainWithClass.py
@@ -1,19 +1,6 @@
 import math
 
 class Complex():
-  def __init__(self,real:float,imaginary:float):
-    self.Re = real
-    self.Im = imaginary
-    self.r = math.sqrt(pow(real,2)+pow(imaginary,2))
-    if imaginary != 0:
-      if imaginary > 0:
-          self.theta =  math.acos(real/(math.sqrt(pow(real,2)+pow(imaginary,2))))
-      else:
-        self.theta =  2*math.pi-math.acos(real/(math.sqrt(pow(real,2)+pow(imaginary,2))))
-    else:
-      self.theta = 0
-    self.thetaDEG = math.degrees(self.theta)
-
   def rTheta(self):
     self.r = math.sqrt(pow(self.Re,2)+pow(self.Im,2))
     if self.Im != 0:
@@ -24,6 +11,12 @@ class Complex():
     else:
       self.theta = 0
     self.thetaDEG = math.degrees(self.theta)
+    return self
+  
+  def __init__(self,real:float,imaginary:float):
+    self.Re = real
+    self.Im = imaginary
+    self.rTheta()
 
   def __str__(self):
     if self.Re == 0:
@@ -51,66 +44,66 @@ class Complex():
 
   def __add__(self,other):
     if type(other) == Complex:
-      return Complex(self.Re+other.Re,self.Im+other.Im)
+      return Complex(self.Re+other.Re,self.Im+other.Im).rTheta()
     else:
-      return Complex(self.Re+other,self.Im)
+      return Complex(self.Re+other,self.Im).rTheta()
     
 
   def __radd__(self,other):
-    return Complex(self.Re+other,self.Im)
+    return Complex(self.Re+other,self.Im).rTheta()
 
   def __sub__(self,other):
     if type(other) == Complex:
-      return Complex(self.Re-other.Re,self.Im-other.Im)
+      return Complex(self.Re-other.Re,self.Im-other.Im).rTheta()
     else:
-      return Complex(self.Re-other,self.Im)
+      return Complex(self.Re-other,self.Im).rTheta()
 
   def __rsub__(self,other):
-    return Complex(-1*(self.Re-other),-1*(self.Im))
+    return Complex(-1*(self.Re-other),-1*(self.Im)).rTheta()
   
 
   def __mul__(self,other):
     if type(other) == Complex:
-      return Complex(self.Re*other.Re-self.Im*other.Im, self.Im*other.Re+self.Re*other.Im)
+      return Complex(self.Re*other.Re-self.Im*other.Im, self.Im*other.Re+self.Re*other.Im).rTheta()
     else:
-      return Complex(self.Re*other,self.Im*other)
+      return Complex(self.Re*other,self.Im*other).rTheta()
   
   def __rmul__(self,other):
-    return Complex(self.Re*other,self.Im*other)
+    return Complex(self.Re*other,self.Im*other).rTheta()
 
   def __truediv__(self,other):
     if type(other) == Complex:
       BDash = math.pow(other.Re,2) + math.pow(other.Im,2)
       ADash = self * Complex(other.Re, -1*other.Im)
-      return Complex(ADash.Re/BDash, ADash.Im/BDash)
+      return Complex(ADash.Re/BDash, ADash.Im/BDash).rTheta()
     else:
-      return Complex(self.Re/other,self.Im/other)
+      return Complex(self.Re/other,self.Im/other).rTheta()
   
   def __rtruediv__(self,other):
     BDash = math.pow(self.Re,2) + math.pow(self.Im,2)
     ADash = other * Complex(self.Re, -1*self.Im)
-    return Complex(ADash.Re/BDash, ADash.Im/BDash)
+    return Complex(ADash.Re/BDash, ADash.Im/BDash).rTheta()
   
   def __pow__(self,other):
     if type(other) == Complex:
       x = (self.r**other.Im)*(math.e**(other.Re*self.theta))
       y = (self.r**other.Re)*(math.e**(-1*self.theta*other.Im))
-      return Complex(y*math.cos(math.log(x)),y*math.sin(math.log(x)))
+      return Complex(y*math.cos(math.log(x)),y*math.sin(math.log(x))).rTheta()
     else:
       r = self.r**other
       theta = self.theta*other
-      return Complex(r*math.cos(theta),r*math.sin(theta))
+      return Complex(r*math.cos(theta),r*math.sin(theta)).rTheta()
     
   def __rpow__(self,other):
-    return Complex(other,0)**self
+    return (Complex(other,0)**self).rTheta()
   
   def __floordiv__(self,other):
     ans = self/other
-    return Complex(math.floor(ans.Re),math.floor(ans.Im))
+    return Complex(math.floor(ans.Re),math.floor(ans.Im)).rTheta()
   
   def __rfloordiv__(self,other):
     ans = self/other
-    return Complex(math.floor(ans.Re),math.floor(ans.Im))
+    return Complex(math.floor(ans.Re),math.floor(ans.Im)).rTheta()
   
   def __mod__(self,other):
     pass
@@ -118,6 +111,6 @@ class Complex():
 
 
 a = 3
-b = Complex(5,45)
+b = Complex(5,2)
 
-print(a//b)
+print((a+b).theta)
