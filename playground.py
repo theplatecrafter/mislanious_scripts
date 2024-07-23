5ecc8aaaf0ffd3dbaff2899f7cb2db5c26809c11
theplatecrafter <theplatecrater@gmail.com>
2024-07-23 12:57:16 +0900

    wip

--- playground.py
+++ playground.py
@@ -3,9 +3,4 @@ import time_capsule_handler.main as tch
 import random_functions.main as rf
 #import ultimate_tick_tack_toe.game as uttt
 
-file_list = rf.getRandomFiles([
-    tch.TimeCapsulePWDLinux["other"]
-],["image","video","audio"],10)
-
-for i in file_list:
-    rf.open_on_web(i)
+rf.open_on_web("/mnt/c/Users/toren/Downloads/test.ods")
