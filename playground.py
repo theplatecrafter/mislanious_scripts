39cd7e23bce0c8717c31595d63121bb94c5c6660
theplatecrafter <theplatecrafter@gmail.com>
2024-10-05 15:12:49 +0900

    ww

--- playground.py
+++ playground.py
@@ -4,8 +4,8 @@ import random_functions.main as rf
 #import ultimate_tick_tack_toe.game as uttt
 
 
-#tch.time_capsule_handler("/media/hans/Torens_Time_Capsle")
-#exit()
+tch.time_capsule_handler("/media/hans/Torens_Time_Capsle")
+exit()
 
 n = 0
 path = "/media/hans/Torens_Time_Capsle/7) 2024/2024-5-31 体育祭/from my camera (with リアル麻雀打ち上げ）"
