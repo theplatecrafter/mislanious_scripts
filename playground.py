4027c565d347235aed5bff34605960918604433b
Hans <theplatecrafter@gmail.com>
2025-05-02 01:51:49 +0900

    wip

--- playground.py
+++ playground.py
@@ -6,8 +6,4 @@ import random_functions.main as rf
 import numpy as np
 
 
-
-dir = "/mnt/f/8) 2025/2025-4-6 Play with friends at Komazawa"
-#dir = "/mnt/c/Users/toren/Pictures/test"
-
-rf.remove_repeated_images(dir,printDeets=True)
+rf.GodsPanel("inputs/GodsPanelPresets/Random.json")
